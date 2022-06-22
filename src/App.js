498c0e5b9425520e2ced0f9b4c15cc64d4220278
Tony Bengué <tonybengue@hotmail.fr>
2022-06-22 20:47:27 +0000

    Add a form component

--- src/App.js
+++ src/App.js
@@ -19,11 +19,49 @@ function ListFontAwesome() {
   )
 }
 
+function MyForm() {
+  const [inputs, setInputs] = useState({});
+
+  const handleChange = ({ target }) => {
+    const name = target.name
+    const value = target.value
+    setInputs(values => ({ ...values, [name]: value }))
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>Enter your name:
+        <input
+          type="text"
+          name="username"
+          value={inputs.username || ""}
+          onChange={handleChange}
+        />
+      </label>
+      <label>Enter your age:
+        <input
+          type="number"
+          name="age"
+          value={inputs.age || ""}
+          onChange={handleChange}
+        />
+      </label>
+      <input type="submit" />
+    </form>
+  )
+}
+
 function App() {
   return (
     <div className="App">
       {
-        ListFontAwesome()
+        //ListFontAwesome()
+        MyForm()
+
       }
      </div>
 
